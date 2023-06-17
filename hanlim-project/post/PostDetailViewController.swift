26f9fe7dcae46861f48ec257e36defb00e791e65
goldsergeant <rmagksfla000@gmail.com>
2023-06-17 12:23:56 +0900

    feat: UI of PostDetailView

---
+++ hanlim-project/post/PostDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PostDetailViewController.swift
+//  hanlim-project
+//
+//  Created by 이정연 on 2023/06/16.
+//
+
+import UIKit
+
+class PostDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
